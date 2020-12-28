2a1098134b82dc72992efa3d376bb4d1f7fc4173
znol1 <57703588+znol1@users.noreply.github.com>
2020-12-28 23:20:41 +0300

    new app added

---
+++ ami/lib/myDay.dart
@@ -0,0 +1,41 @@
+import 'package:ami/widgets/dayWidget.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class MyDay extends StatefulWidget {
+  @override
+  _MyDayState createState() => _MyDayState();
+}
+
+class _MyDayState extends State<MyDay> {
+  MediaQueryData mediaQuery;
+  var nightStart = 0.0;
+  var nightEnd = 0.3;
+  var time = DateFormat('HH:mm').format(DateTime.now());
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ы'),
+      ),
+      body: Center(
+        child: Column(children: [
+          Container(
+            padding: EdgeInsets.only(top: mediaQuery.size.height / 20),
+            child: Text(time, style: TextStyle(fontSize: 30)),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: mediaQuery.size.height / 20),
+            child: InteractiveViewer(
+              child: CustomPaint(
+                painter: DayWidget(nightStart, nightEnd),
+                size: Size(300, 300),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
