1cc52bb645ea33dd9a9aa918902852832ea3f1cf
znol1 <57703588+znol1@users.noreply.github.com>
2020-12-29 00:21:52 +0300

    time picker started

--- ami/lib/myDay.dart
+++ ami/lib/myDay.dart
@@ -1,4 +1,6 @@
+import 'package:ami/widgets/cupertino_picker.dart';
 import 'package:ami/widgets/dayWidget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -9,9 +11,12 @@ class MyDay extends StatefulWidget {
 
 class _MyDayState extends State<MyDay> {
   MediaQueryData mediaQuery;
-  var nightStart = 0.0;
-  var nightEnd = 0.3;
+  var nightStart = 0.0 / 24.0;
+  var nightEnd = 8 / 24;
   var time = DateFormat('HH:mm').format(DateTime.now());
+  int selectedValue;
+  showPicker() {}
+
   @override
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context);
@@ -32,6 +37,32 @@ class _MyDayState extends State<MyDay> {
                 painter: DayWidget(nightStart, nightEnd),
                 size: Size(300, 300),
               ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: mediaQuery.size.height / 20),
+            child: FlatButton(
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0)),
+              color: Colors.deepPurple,
+              onPressed: showPicker,
+              child: Text(
+                'Изменить ночное время',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Picker(),
+                Picker(),
+                Text(':'),
+                Picker(),
+                Picker(),
+              ],
             ),
           ),
         ]),
---
+++ ami/lib/widgets/cupertino_picker.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Picker extends StatefulWidget {
+  @override
+  _PickerState createState() => _PickerState();
+}
+
+class _PickerState extends State<Picker> {
+  int selectedValue;
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
+    return Container(
+      width: mediaQuery.size.width / 5,
+      child: CupertinoPicker(
+        backgroundColor: Colors.white,
+        onSelectedItemChanged: (value) {
+          setState(() {
+            selectedValue = value;
+          });
+        },
+        itemExtent: 32.0,
+        children: const [
+          Text('0'),
+          Text('1'),
+          Text('2'),
+          Text('3'),
+          Text('4'),
+          Text('5'),
+          Text('6'),
+          Text('7'),
+          Text('8'),
+          Text('9'),
+          Text('10'),
+          Text('11'),
+          Text('12'),
+          Text('13'),
+          Text('14'),
+          Text('15'),
+          Text('16'),
+          Text('17'),
+          Text('18'),
+          Text('19'),
+          Text('20'),
+          Text('21'),
+          Text('22'),
+          Text('23'),
+        ],
+      ),
+    );
+  }
+}
