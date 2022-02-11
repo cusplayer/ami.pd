833dc6af0ef31218b021210caec33151280e28cf
znol1 <57703588+znol1@users.noreply.github.com>
2022-02-11 22:35:54 +0300

    before global refactoring

---
+++ ami/lib/providers/diagram_rules.dart
@@ -0,0 +1,9 @@
+import 'package:ami/providers/activities.dart';
+import 'package:flutter/cupertino.dart';
+
+class DiagramRules extends ChangeNotifier {
+  Activities activities;
+  DiagramRules({
+    required this.activities,
+  });
+}
